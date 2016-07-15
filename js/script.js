84a275e8b265cdf24b297bc2c20b334a2215bf1c
aleeex <akang2@gmail.com>
2016-07-15 15:59:34 -0600

    created index and script file and added first code

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var todos = ['item1','item2','item3'];
+
+//display all the todos
+function displayAll() {
+  console.log(todos);
+}
+
+//add new items
+function addItem(item) {
+  todos.push(item);
+}
+
+//change todo item
+function changeItem(position, newValue) {
+  todos[position] = newValue;
+}
+
+//delete an item
+function deleteItem(position) {
+  todos.splice(position,1);
+}
+
+displayAll();
+changeItem(3,'yo');
+displayAll();
