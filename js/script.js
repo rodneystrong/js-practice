c3428e0e34aaf69cf468ce30203b9bce2c0e0eed
aleeex <akang2@gmail.com>
2016-07-15 16:02:55 -0600

    put todolist into object

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-var todos = ['item1','item2','item3'];
+//store todos in object
+var todoList = {
+    todos: ['item1','item2','item3']
+};
 
-//display all the todos
-function displayAll() {
-  console.log(todos);
-}
-
-//add new items
-function addItem(item) {
-  todos.push(item);
-}
-
-//change todo item
-function changeItem(position, newValue) {
-  todos[position] = newValue;
-}
-
-//delete an item
-function deleteItem(position) {
-  todos.splice(position,1);
-}
-
-displayAll();
-changeItem(3,'yo');
-displayAll();
+//
+// //display all the todos
+// function displayAll() {
+//   console.log(todos);
+// }
+//
+// //add new items
+// function addItem(item) {
+//   todos.push(item);
+// }
+//
+// //change todo item
+// function changeItem(position, newValue) {
+//   todos[position] = newValue;
+// }
+//
+// //delete an item
+// function deleteItem(position) {
+//   todos.splice(position,1);
+// }
+//
+// displayAll();
+// changeItem(3,'yo');
+// displayAll();
