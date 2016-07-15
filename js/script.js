b43f30d0231c43553d145a72ddc95df71f3009e5
aleeex <akang2@gmail.com>
2016-07-15 16:17:18 -0600

    added code to change item and remove item

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 //store todos in object
 var todoList = {
     todos: ['item1','item2','item3'],
+    //display todo items
     displayItems: function() {
       console.log(this.todos);
     },
+    //add todo item
     addItem: function(item) {
       this.todos.push(item);
       this.displayItems();
+    },
+    //change todo item
+    //takes in # param (position) and any value type param (newValue)
+    changeItem: function(position, newValue) {
+      this.todos[position] = newValue;
+    },
+    //delete todo item
+    deleteItem: function(position) {
+      this.todos.splice(position, 1);
     }
 };
 
